4d6be3219393deb99eecf0f77319fd7f4eb4233d
mjmccoy <mjmccoy@stanford.edu>
2024-01-19 13:50:30 -0800

    toggle quantiles

--- R/sizeR_plot.R
+++ R/sizeR_plot.R
@@ -7,6 +7,7 @@
 #' @param by_sample should data be grouped by sample?
 #' @param categorical is group categorical?
 #' @param type geom type (i.e. bar, tile, line)
+#' @param quantiles should x and y values be quantiles?
 #'
 #' @return returns a sizeR plot
 #' @export
@@ -21,6 +22,7 @@
 #' data(example_quantitative_feature_binomial_test_output)
 #' sizeR_plot(
 #'   data.df = example_quantitative_feature_binomial_test_output,
+#'   quantiles = FALSE,
 #'   type = "bar")
 #'
 #' # quantitative feature by sample
@@ -28,12 +30,14 @@
 #' sizeR_plot(
 #'   data.df = example_quantitative_feature_by_sample_binomial_test_output,
 #'   by_sample = TRUE,
+#'   quantiles = FALSE,
 #'   type = "bar")
 #'
 #' # categorical feature
 #' data(example_categorical_binomial_test_output)
 #' sizeR_plot(
 #'   data.df = example_categorical_binomial_test_output, categorical = TRUE,
+#'   quantiles = FALSE,
 #'   type = "bar")
 #'
 #' ####################
@@ -43,18 +47,21 @@
 #' data(example_quantitative_feature_binomial_test_output)
 #' sizeR_plot(
 #'   data.df = example_quantitative_feature_binomial_test_output,
+#'   quantiles = FALSE,
 #'   type = "tile")
 #'
 #' data(example_quantitative_feature_by_sample_binomial_test_output)
 #' sizeR_plot(
 #'   data.df = example_quantitative_feature_by_sample_binomial_test_output,
 #'   by_sample = TRUE,
+#'   quantiles = FALSE,
 #'   type = "tile")
 #'
 #' data(example_categorical_binomial_test_output)
 #' sizeR_plot(
 #'   data.df = example_categorical_binomial_test_output,
 #'   categorical = TRUE,
+#'   quantiles = FALSE,
 #'   type = "tile")
 #'
 #' ###################
@@ -64,6 +71,7 @@
 #' data(example_quantitative_feature_by_sample_binomial_test_input)
 #' sizeR_plot(
 #'   data.df = example_quantitative_feature_by_sample_binomial_test_input,
+#'   quantiles = FALSE,
 #'   type = "line") +
 #'   scale_color_manual(values = c("condition1.r1" = "firebrick", "condition1.r2" = "firebrick", "condition2.r1" = "black", "condition2.r2" = "black"))
 
@@ -71,9 +79,10 @@ sizeR_plot <- function(
     data.df,
     by_sample = FALSE,
     categorical = FALSE,
+    quantiles = TRUE,
     type){
   length_range <- feature_range <- n <- mean_n <- sign <- name <- group <- NULL
-  if(by_sample == TRUE & categorical == FALSE & type == "bar"){
+  if(by_sample == TRUE & categorical == FALSE & quantiles == FALSE & type == "bar"){
     data.df %>%
       ggplot(aes(x = length_range, y = n, fill = sign)) +
       geom_col(col = "black") +
@@ -88,7 +97,28 @@ sizeR_plot <- function(
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
       labs(x = "Gene size (kb)", y = "Genes") +
       facet_grid(feature_range~name)
-  } else if(by_sample == FALSE & categorical == FALSE & type == "bar"){
+  } else if(by_sample == TRUE & categorical == FALSE & quantiles == TRUE & type == "bar"){
+    data.df %>%
+      mutate(
+        length_range = as.integer(length_range),
+        feature_range = as.integer(feature_range)) %>%
+      ggplot(aes(x = as.factor(length_range), y = n, fill = sign)) +
+      geom_col(col = "black") +
+      geom_hline(data = data.df %>%
+                   mutate(
+                     length_range = as.integer(length_range),
+                     feature_range = as.integer(feature_range)) %>%
+                   group_by(name, feature_range) %>%
+                   summarise(mean_n = mean(n)),
+                 aes(yintercept = mean_n),
+                 linetype = "dotted") +
+      scale_fill_manual(
+        name = "Binomial", values = c("+" = "firebrick", "-" = "skyblue")) +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      labs(x = "Gene size quantiles", y = "Genes") +
+      facet_grid(feature_range~name)
+  } else if(by_sample == FALSE & categorical == FALSE & quantiles == FALSE & type == "bar"){
     data.df %>%
       ggplot(aes(x = length_range, y = n, fill = sign)) +
       geom_col(col = "black") +
@@ -103,7 +133,28 @@ sizeR_plot <- function(
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
       labs(x = "Gene size (kb)", y = "Genes") +
       facet_grid(~feature_range)
-  } else if(categorical == TRUE & type == "bar"){
+  } else if(by_sample == FALSE & categorical == FALSE & quantiles == TRUE & type == "bar"){
+    data.df %>%
+      mutate(
+        length_range = as.integer(length_range),
+        feature_range = as.integer(feature_range)) %>%
+      ggplot(aes(x = as.factor(length_range), y = n, fill = sign)) +
+      geom_col(col = "black") +
+      geom_hline(data = data.df %>%
+                   mutate(
+                     length_range = as.integer(length_range),
+                     feature_range = as.integer(feature_range)) %>%
+                   group_by(feature_range) %>%
+                   summarise(mean_n = mean(n)),
+                 aes(yintercept = mean_n),
+                 linetype = "dotted") +
+      scale_fill_manual(
+        name = "Binomial", values = c("+" = "firebrick", "-" = "skyblue")) +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      labs(x = "Gene size quantiles", y = "Genes") +
+      facet_grid(~feature_range)
+  } else if(categorical == TRUE & quantiles == FALSE & type == "bar"){
     data.df %>%
       ggplot(aes(x = length_range, y = n, fill = sign)) +
       geom_col(col = "black") +
@@ -118,7 +169,24 @@ sizeR_plot <- function(
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
       labs(x = "Gene size (kb)", y = "Genes") +
       facet_grid(~group)
-  } else if(by_sample == TRUE & categorical == FALSE & type == "tile"){
+  } else if(categorical == TRUE & quantiles == TRUE & type == "bar"){
+    data.df %>%
+      mutate(length_range = as.integer(length_range)) %>%
+      ggplot(aes(x = as.factor(length_range), y = n, fill = sign)) +
+      geom_col(col = "black") +
+      geom_hline(data = data.df %>%
+                   mutate(length_range = as.integer(length_range)) %>%
+                   group_by(group) %>%
+                   summarise(mean_n = mean(n)),
+                 aes(yintercept = mean_n),
+                 linetype = "dotted") +
+      scale_fill_manual(
+        name = "Binomial", values = c("+" = "firebrick", "-" = "skyblue")) +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      labs(x = "Gene size quantiles", y = "Genes") +
+      facet_grid(~group)
+  } else if(by_sample == TRUE & categorical == FALSE & quantiles == FALSE & type == "tile"){
     data.df %>%
       ggplot(aes(x = length_range, y = feature_range, fill = n)) +
       geom_tile() +
@@ -128,7 +196,20 @@ sizeR_plot <- function(
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
       labs(x = "Gene size (kb)", y = "Feature") +
       facet_grid(~name)
-  } else if(by_sample == FALSE & categorical == FALSE & type == "tile"){
+  } else if(by_sample == TRUE & categorical == FALSE & quantiles == TRUE & type == "tile"){
+    data.df %>%
+      mutate(
+        length_range = as.integer(length_range),
+        feature_range = as.integer(feature_range)) %>%
+      ggplot(aes(x = as.factor(length_range), y = as.factor(feature_range), fill = n)) +
+      geom_tile() +
+      geom_text(aes(label = sign)) +
+      scale_fill_distiller(name = "Genes", palette = "RdGy") +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      labs(x = "Gene size quantiles", y = "Feature quantiles") +
+      facet_grid(~name)
+  } else if(by_sample == FALSE & categorical == FALSE & quantiles == FALSE & type == "tile"){
     data.df %>%
       ggplot(aes(x = length_range, y = feature_range, fill = n)) +
       geom_tile() +
@@ -138,7 +219,20 @@ sizeR_plot <- function(
       theme_bw() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
       labs(x = "Gene size (kb)", y = "Feature")
-  } else if(categorical == TRUE & type == "tile"){
+  } else if(by_sample == FALSE & categorical == FALSE & quantiles == TRUE & type == "tile"){
+    data.df %>%
+      mutate(
+        length_range = as.integer(length_range),
+        feature_range = as.integer(feature_range)) %>%
+      ggplot(aes(x = as.factor(length_range), y = as.factor(feature_range), fill = n)) +
+      geom_tile() +
+      geom_text(aes(label = sign)) +
+      scale_fill_distiller(name = "Genes", palette = "RdGy") +
+      coord_equal() +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      labs(x = "Gene size quantiles", y = "Feature quantiles")
+  } else if(categorical == TRUE & quantiles == FALSE & type == "tile"){
     data.df %>%
       ggplot(aes(x = length_range, y = reorder(group, n), fill = n)) +
       geom_tile() +
@@ -148,7 +242,19 @@ sizeR_plot <- function(
       theme_bw() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
       labs(x = "Gene size (kb)", y = "Feature")
-  } else if(type == "line"){
+  } else if(categorical == TRUE & quantiles == TRUE & type == "tile"){
+    data.df %>%
+      mutate(
+        length_range = as.integer(length_range)) %>%
+      ggplot(aes(x = as.factor(length_range), y = reorder(group, n), fill = n)) +
+      geom_tile() +
+      geom_text(aes(label = sign)) +
+      scale_fill_distiller(name = "Genes", palette = "RdGy") +
+      coord_equal() +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      labs(x = "Gene size quantiles", y = "Feature")
+  } else if(quantiles == FALSE & type == "line"){
     data.df %>%
       pivot_longer(!c(gene_id, length)) %>%
       mutate(length_bins = ntile(length, n = 100)) %>%
@@ -164,5 +270,15 @@ sizeR_plot <- function(
       labs(x = "Gene size (kb)", y = "Gene expression") +
       scale_x_log10() +
       annotation_logticks(sides = "b")
+  } else if(quantiles == TRUE & type == "line"){
+    data.df %>%
+      pivot_longer(!c(gene_id, length)) %>%
+      mutate(length_bins = ntile(length, n = 100)) %>%
+      ggplot(aes(x = length_bins, y = sqrt(value), col = name, group = name)) +
+      stat_summary(geom = "ribbon", fun.data = mean_cl_normal, alpha = 0.1, aes(col = NULL)) +
+      stat_summary(geom = "point", size = 0.1) +
+      stat_summary(geom = "line") +
+      theme_bw() +
+      labs(x = "Gene size quantiles", y = "Gene expression")
   }
 }
